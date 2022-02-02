95ccf8e2bc909f0b40a478e8819843b02aa2da07
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-02-02 14:25:33 +0100

    main.js to gitignore

--- task.sh
+++ task.sh
@@ -2,7 +2,7 @@ while true; do
 	read -p "Do you wish to run in watch mode? (y/n)" yn
     case $yn in
         [Yy]* ) webpack --watch --mode development; break;;
-        [Nn]* ) webpack --mode development;break;;
+        [Nn]* ) webpack --mode development; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
